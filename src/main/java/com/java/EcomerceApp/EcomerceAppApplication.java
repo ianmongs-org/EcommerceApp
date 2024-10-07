2d8e1c8b325990a7cd5a2e4e137b6bf8f41fb0b7
Dancan254 <dancanian25@gmail.com>
2024-10-07 13:26:46 -0700

    Initial commit

---
+++ src/main/java/com/java/EcomerceApp/EcomerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.java.EcomerceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceAppApplication.class, args);
+	}
+
+}
