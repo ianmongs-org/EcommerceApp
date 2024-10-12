c6be19acf1d1b94c8163e6dddac7fb79ec461731
Dancan254 <dancanian25@gmail.com>
2024-10-12 23:35:58 +0300

    refactor

--- src/main/java/com/java/EcomerceApp/EcommerceAppApplication.java
+++ src/main/java/com/java/EcomerceApp/EcommerceAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcomerceAppApplication {
+public class EcommerceAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcomerceAppApplication.class, args);
+		SpringApplication.run(EcommerceAppApplication.class, args);
 	}
 
 }
