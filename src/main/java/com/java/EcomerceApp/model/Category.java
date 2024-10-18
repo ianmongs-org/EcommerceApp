04ec13f646e5677c4a4bed2537808bf35256a7bc
Dancan254 <dancanian25@gmail.com>
2024-10-18 11:37:10 +0300

    adding validations in the category entity

--- src/main/java/com/java/EcomerceApp/model/Category.java
+++ src/main/java/com/java/EcomerceApp/model/Category.java
@@ -21,5 +21,4 @@ public class Category {
     @NotBlank
     @Size(min = 3, max = 20, message = "Name must be at least 3 characters long")
     private String name;
-    private String description;
 }
