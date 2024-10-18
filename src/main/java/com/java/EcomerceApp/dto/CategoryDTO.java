c06527f289f3ee6fdf73cb5abc1490355ba0e123
Dancan254 <dancanian25@gmail.com>
2024-10-18 11:35:26 +0300

    setting up the category dto and the response classes

---
+++ src/main/java/com/java/EcomerceApp/dto/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.java.EcomerceApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CategoryDTO {
+    private Long categoryId;
+    private String categoryName;
+}
