5f419b397dc8b4aeeb4eec521c31816110f3cdf9
Dancan254 <dancanian25@gmail.com>
2024-10-24 00:16:46 +0300

    custom method implementation for method to find category order by price in ascending order

--- src/main/java/com/java/EcomerceApp/repository/ProductRepository.java
+++ src/main/java/com/java/EcomerceApp/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.java.EcomerceApp.repository;
 
+import com.java.EcomerceApp.model.Category;
 import com.java.EcomerceApp.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByProductName(String productName);
+
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 }
