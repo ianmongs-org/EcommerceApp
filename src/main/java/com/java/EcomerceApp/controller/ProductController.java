dc9db67c3aee8737f3a4b8a0705c78bf5bc2aa2d
Dancan254 <dancanian25@gmail.com>
2024-10-23 10:21:55 +0300

    feat: creating the endpoint to add product

--- src/main/java/com/java/EcomerceApp/controller/ProductController.java
+++ src/main/java/com/java/EcomerceApp/controller/ProductController.java
@@ -1,9 +1,23 @@
 package com.java.EcomerceApp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.java.EcomerceApp.dto.ProductDTO;
+import com.java.EcomerceApp.model.Product;
+import com.java.EcomerceApp.service.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/admin/category/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO,
+                                                 @PathVariable Long categoryId) {
+        return new ResponseEntity<>(productService.addProduct(productDTO, categoryId),HttpStatus.CREATED);
+    }
 }
