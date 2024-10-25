1c60138b5acce18856801f541bf5ed8203615395
Dancan254 <dancanian25@gmail.com>
2024-10-25 17:14:52 +0300

    refactor: Update ProductService interface to include updateProductImage method

--- src/main/java/com/java/EcomerceApp/service/product/ProductService.java
+++ src/main/java/com/java/EcomerceApp/service/product/ProductService.java
@@ -3,6 +3,9 @@ package com.java.EcomerceApp.service.product;
 import com.java.EcomerceApp.dto.ProductDTO;
 import com.java.EcomerceApp.dto.ProductResponse;
 import com.java.EcomerceApp.model.Product;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProductService {
     ProductDTO addProduct(ProductDTO productDTO, Long categoryId);
@@ -15,4 +18,6 @@ public interface ProductService {
 
     ProductDTO updateProduct(Long productId, ProductDTO productDTO);
     String deleteProduct(Long productId);
+
+    ProductDTO updateProductImage(Long productId, MultipartFile file) throws IOException;
 }
