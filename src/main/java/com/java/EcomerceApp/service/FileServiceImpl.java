020dc356161d178b630100a67df79bef1916c795
Dancan254 <dancanian25@gmail.com>
2024-10-27 23:51:05 +0300

    feat: Add @Service annotation to FileServiceImpl

--- src/main/java/com/java/EcomerceApp/service/FileServiceImpl.java
+++ src/main/java/com/java/EcomerceApp/service/FileServiceImpl.java
@@ -1,5 +1,6 @@
 package com.java.EcomerceApp.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -9,6 +10,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
+@Service
 public class FileServiceImpl implements FileService{
 
     public String uploadImage(String path, MultipartFile file) throws IOException {
