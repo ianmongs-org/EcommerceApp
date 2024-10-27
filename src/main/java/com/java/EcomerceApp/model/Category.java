b2c1db54b7615a0ed2d83abb83dfdac4758b0272
Dancan254 <dancanian25@gmail.com>
2024-10-28 00:48:45 +0300

    refactor: Add one-to-many relationship between Category and Product models

--- src/main/java/com/java/EcomerceApp/model/Category.java
+++ src/main/java/com/java/EcomerceApp/model/Category.java
@@ -1,14 +1,13 @@
 package com.java.EcomerceApp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -21,4 +20,7 @@ public class Category {
     @NotBlank
     @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    List<Product> products;
 }
