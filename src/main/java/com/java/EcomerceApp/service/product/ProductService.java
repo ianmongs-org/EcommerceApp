3686f44b2285b41c103115256c06eaadaf3d15d1
Dancan254 <dancanian25@gmail.com>
2024-10-28 09:44:22 +0300

    refactor: Update ProductService to include pagination and sorting options for retrieving products by category and keyword

--- src/main/java/com/java/EcomerceApp/service/product/ProductService.java
+++ src/main/java/com/java/EcomerceApp/service/product/ProductService.java
@@ -9,15 +9,10 @@ import java.io.IOException;
 
 public interface ProductService {
     ProductDTO addProduct(ProductDTO productDTO, Long categoryId);
-
-    ProductResponse getAllProducts();
-
-    ProductResponse getProductsByCategory(Long categoryId);
-
-    ProductResponse getProductsByKeyword(String keyword);
-
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    ProductResponse getProductsByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    ProductResponse getProductsByKeyword(String keyword, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     ProductDTO updateProduct(Long productId, ProductDTO productDTO);
     String deleteProduct(Long productId);
-
     ProductDTO updateProductImage(Long productId, MultipartFile file) throws IOException;
 }
