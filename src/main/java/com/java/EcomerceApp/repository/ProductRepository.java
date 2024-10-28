e15e6dde8ec765b1ddd81cc1c461ab16e5a92626
Dancan254 <dancanian25@gmail.com>
2024-10-28 09:43:44 +0300

    refactor: Update ProductRepository to use pagination for category and keyword searches

--- src/main/java/com/java/EcomerceApp/repository/ProductRepository.java
+++ src/main/java/com/java/EcomerceApp/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.java.EcomerceApp.repository;
 
 import com.java.EcomerceApp.model.Category;
 import com.java.EcomerceApp.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByProductName(String productName);
 
-    List<Product> findByCategoryOrderByPriceAsc(Category category);
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageable);
 
-    List<Product> findByProductNameLikeIgnoreCase(String keyword);
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageable);
 }
