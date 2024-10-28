b89277580ef7049d1eb7bc37436839fd110423dd
Dancan254 <dancanian25@gmail.com>
2024-10-28 19:03:47 +0300

    refactor: Add User model for user information and authentication

---
+++ src/main/java/com/java/EcomerceApp/model/User.java
@@ -0,0 +1,55 @@
+package com.java.EcomerceApp.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    @Column(name = "username", unique = true)
+    private String username;
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Email must be between 3 and 50 characters")
+    @Email
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 120, message = "Password must be greater than 8characters")
+    private String password;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "roleId"))
+    Set<Role> roles = new HashSet<>();
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private Set<Product> products;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_address",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private List<Address> addresses;
+}
