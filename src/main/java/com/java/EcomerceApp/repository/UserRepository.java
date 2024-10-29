1e2359113a5cfc7d451ab03111f06ace73b966e2
Dancan254 <dancanian25@gmail.com>
2024-10-29 22:17:55 +0300

    refactor: Add UserRepository for user data access

---
+++ src/main/java/com/java/EcomerceApp/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.java.EcomerceApp.repository;
+
+import com.java.EcomerceApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
