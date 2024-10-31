6a5afbba70ee12d84366097970d719ae38890782
Dancan254 <dancanian25@gmail.com>
2024-10-31 21:41:31 +0300

    refactor: Add constructor to User class for initializing username, email, and password

--- src/main/java/com/java/EcomerceApp/model/User.java
+++ src/main/java/com/java/EcomerceApp/model/User.java
@@ -4,10 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.List;
@@ -52,4 +49,10 @@ public class User {
                 joinColumns = @JoinColumn(name = "user_id"),
                 inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses;
+
+    public User(String username,String email, String encode) {
+        this.username = username;
+        this.email = email;
+        this.password = encode;
+    }
 }
