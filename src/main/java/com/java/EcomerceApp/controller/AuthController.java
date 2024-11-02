3a5bd417210d78926b1085508f19dd24c947706e
Dancan254 <dancanian25@gmail.com>
2024-11-02 14:34:28 +0300

    feat: Add authentication endpoints and controllers for user sign-in and sign-up

--- src/main/java/com/java/EcomerceApp/controller/AuthController.java
+++ src/main/java/com/java/EcomerceApp/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.java.EcomerceApp.model.Role;
 import com.java.EcomerceApp.model.User;
 import com.java.EcomerceApp.repository.RoleRepository;
 import com.java.EcomerceApp.repository.UserRepository;
+import com.java.EcomerceApp.security.services.AuthService;
 import com.java.EcomerceApp.security.services.UserDetailsImpl;
 import com.java.EcomerceApp.security.utils.JwtUtils;
 import jakarta.validation.Valid;
@@ -41,77 +42,33 @@ public class AuthController {
     private PasswordEncoder encoder;
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    AuthService authService;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest){
-        Authentication authentication;
-        try{
-            authentication = authenticationManager
-                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-        }catch(AuthenticationException e){
+        try {
+            UserInfoResponse loginResponse = authService.authenticateUser(loginRequest);
+            return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+        } catch (AuthenticationException e) {
             Map<String, Object> map = new HashMap<>();
             map.put("message", "Bad credentials");
             map.put("status", false);
             return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
         }
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        String jwt = jwtUtils.generateTokenFromUsername(userDetails);
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority()).toList();
-        UserInfoResponse loginResponse = new UserInfoResponse(userDetails.getId(),jwt, userDetails.getUsername(), roles);
-        return new ResponseEntity<>(loginResponse, HttpStatus.OK);
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        try {
+            return new ResponseEntity<>(authService.registerUser(signUpRequest), HttpStatus.OK);
+        } catch (RuntimeException e) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", e.getMessage());
+            map.put("status", false);
+            return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
         }
-
-        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
-        }
-
-        // Create new user's account
-        User user = new User(signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
-                encoder.encode(signUpRequest.getPassword()));
-
-        Set<String> strRoles = signUpRequest.getRole();
-        Set<Role> roles = new HashSet<>();
-
-        if (strRoles == null) {
-            Role userRole = roleRepository.findByAppRole(AppRole.USER)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(userRole);
-        } else {
-            strRoles.forEach(role -> {
-                switch (role) {
-                    case "admin":
-                        Role adminRole = roleRepository.findByAppRole(AppRole.ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(adminRole);
-
-                        break;
-                    case "seller":
-                        Role modRole = roleRepository.findByAppRole(AppRole.SELLER)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(modRole);
-
-                        break;
-                    default:
-                        Role userRole = roleRepository.findByAppRole(AppRole.USER)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(userRole);
-                }
-            });
-        }
-
-        user.setRoles(roles);
-        userRepository.save(user);
-
-        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
+
     
 }
