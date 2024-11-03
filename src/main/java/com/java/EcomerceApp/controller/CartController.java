ca58e8b56ce6f88470da3e00d3aa8e7ddc934c83
Dancan254 <dancanian25@gmail.com>
2024-11-03 20:44:44 +0300

    chore: Refactor CartController to use correct endpoint for adding products to cart and implement getCart endpoint

--- src/main/java/com/java/EcomerceApp/controller/CartController.java
+++ src/main/java/com/java/EcomerceApp/controller/CartController.java
@@ -1,14 +1,12 @@
 package com.java.EcomerceApp.controller;
 
 import com.java.EcomerceApp.dto.CartDTO;
+
 import com.java.EcomerceApp.service.cart.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -16,9 +14,15 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    @PostMapping("/carts/products/{productId}/quantity/{quantity}")
+    @PostMapping("/cart/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
                                                     @PathVariable Integer quantity) {
-        return new ResponseEntity<>(cartService.addProductToCart(productId, quantity), HttpStatus.OK);
+            CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
+            return new ResponseEntity<>(cartDTO, HttpStatus.OK);
+    }
+    @GetMapping("/carts")
+    public ResponseEntity<CartDTO> getCart(){
+        CartDTO cartDTO = cartService.getCart();
+        return new ResponseEntity<>(cartDTO, HttpStatus.OK);
     }
 }
