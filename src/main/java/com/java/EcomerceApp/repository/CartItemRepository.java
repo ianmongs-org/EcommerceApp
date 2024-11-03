790ddc2aa51e019162a7cce80e93a2abd5aa07ea
Dancan254 <dancanian25@gmail.com>
2024-11-03 11:31:29 +0300

    refactor: Add CartItemRepository for querying cart items by cart ID and product ID

---
+++ src/main/java/com/java/EcomerceApp/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.java.EcomerceApp.repository;
+
+import com.java.EcomerceApp.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @Query("SELECT c FROM CartItem c WHERE c.cart.cartId = ?1 AND c.product.productId = ?2")
+    CartItem findByCartIdAndProductId(Long cartId, Long productId);
+}
