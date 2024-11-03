6d420f3a8afac74c2c918e92d366e27b1f9e9b87
Dancan254 <dancanian25@gmail.com>
2024-11-03 11:30:02 +0300

    refactor: Add Cart model for representing the shopping cart with user, cart items, and total price

---
+++ src/main/java/com/java/EcomerceApp/model/Cart.java
@@ -0,0 +1,28 @@
+package com.java.EcomerceApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
+    private List<CartItem> cartItems;
+
+    private Double totalPrice;
+
+}
