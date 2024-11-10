f2e591edede2a9d6274e0ddafa8b92037b0ec358
Dancan254 <dancanian25@gmail.com>
2024-11-10 22:04:35 +0300

    chore: Update ProductController to handle file uploads and CRUD operations

--- src/main/java/com/java/EcomerceApp/controller/ProductController.java
+++ src/main/java/com/java/EcomerceApp/controller/ProductController.java
@@ -1,23 +1,34 @@
 package com.java.EcomerceApp.controller;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.java.EcomerceApp.config.AppConstants;
 import com.java.EcomerceApp.dto.ProductDTO;
 import com.java.EcomerceApp.dto.ProductResponse;
 import com.java.EcomerceApp.service.product.ProductService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
-
+    @Autowired
     private final ProductService productService;
 
     @PostMapping("/admin/category/{categoryId}/product")
