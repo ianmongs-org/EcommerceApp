0a17189e9c578084281538a27b5d4d00956aa384
Dancan254 <dancanian25@gmail.com>
2024-11-19 21:28:06 +0300

    feat: Add OrderItem model for managing order item data and relationships

---
+++ src/main/java/com/java/EcomerceApp/model/OrderItem.java
@@ -0,0 +1,25 @@
+package com.java.EcomerceApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+    private Integer quantity;
+    private Double productPrice;
+    private Double discount;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
