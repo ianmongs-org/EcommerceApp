7fb60e229b06a5b5abc89f8f60f0eeee76f3b48a
Dancan254 <dancanian25@gmail.com>
2024-11-19 21:27:28 +0300

    feat: Add OrderRequestDTO for encapsulating order request data

---
+++ src/main/java/com/java/EcomerceApp/dto/OrderRequestDTO.java
@@ -0,0 +1,17 @@
+package com.java.EcomerceApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequestDTO {
+    private Long addressId;
+    private String paymentMethod;
+    private String pgName;
+    private String pgPaymentId;
+    private String pgPaymentStatus;
+    private String pgResponseMessage;
+}
