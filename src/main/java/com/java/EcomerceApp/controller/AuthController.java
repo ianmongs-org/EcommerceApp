9ce15fc8628c4a0e0a72b6525854753e0945b66a
Dancan254 <dancanian25@gmail.com>
2024-11-19 17:56:07 +0300

    refactor: Simplify AuthController by removing unused imports and dependencies

--- src/main/java/com/java/EcomerceApp/controller/AuthController.java
+++ src/main/java/com/java/EcomerceApp/controller/AuthController.java
@@ -1,47 +1,27 @@
 package com.java.EcomerceApp.controller;
 
-import com.java.EcomerceApp.dto.LoginRequest;
-import com.java.EcomerceApp.dto.MessageResponse;
-import com.java.EcomerceApp.dto.SignupRequest;
-import com.java.EcomerceApp.dto.UserInfoResponse;
-import com.java.EcomerceApp.model.AppRole;
-import com.java.EcomerceApp.model.Role;
-import com.java.EcomerceApp.model.User;
-import com.java.EcomerceApp.repository.RoleRepository;
-import com.java.EcomerceApp.repository.UserRepository;
-import com.java.EcomerceApp.security.services.AuthService;
-import com.java.EcomerceApp.security.services.UserDetailsImpl;
-import com.java.EcomerceApp.security.utils.JwtUtils;
-import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import com.java.EcomerceApp.dto.LoginRequest;
+import com.java.EcomerceApp.dto.SignupRequest;
+import com.java.EcomerceApp.dto.UserInfoResponse;
+import com.java.EcomerceApp.security.services.AuthService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
-    @Autowired
-    private JwtUtils jwtUtils;
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PasswordEncoder encoder;
-    @Autowired
-    private RoleRepository roleRepository;
     @Autowired
     AuthService authService;
 
