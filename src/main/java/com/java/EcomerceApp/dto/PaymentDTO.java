9709dd1b55031fc44cac3b1207e7ac6afcc63927
Dancan254 <dancanian25@gmail.com>
2024-11-19 21:27:35 +0300

    feat: Add PaymentDTO for encapsulating payment data

---
+++ src/main/java/com/java/EcomerceApp/dto/PaymentDTO.java
@@ -0,0 +1,17 @@
+package com.java.EcomerceApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDTO {
+    private Long paymentId;
+    private String paymentMethod;
+    private String pgPaymentId;
+    private String paymentStatus;
+    private String responseMessage;
+    private Double amount;
+}
