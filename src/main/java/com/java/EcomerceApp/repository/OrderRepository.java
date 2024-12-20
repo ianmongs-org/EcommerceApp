bffdf709ac04d90a2c4b6b1d1b80b9e690338788
Dancan254 <dancanian25@gmail.com>
2024-12-20 17:31:06 +0300

    feat: Add method to check if an order exists by email, address, and order date

--- src/main/java/com/java/EcomerceApp/repository/OrderRepository.java
+++ src/main/java/com/java/EcomerceApp/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.java.EcomerceApp.repository;
 
+import com.java.EcomerceApp.model.Address;
 import com.java.EcomerceApp.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    boolean existsByEmailAndAddressAndOrderDate(String email, Address address, LocalDate now);
 }
