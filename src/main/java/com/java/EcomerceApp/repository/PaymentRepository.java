62b66b7648fa2f54ce8f96fd5279abc07d74c97a
Dancan254 <dancanian25@gmail.com>
2024-12-20 17:31:10 +0300

    feat: Add method to find payment by associated order

--- src/main/java/com/java/EcomerceApp/repository/PaymentRepository.java
+++ src/main/java/com/java/EcomerceApp/repository/PaymentRepository.java
@@ -1,7 +1,9 @@
 package com.java.EcomerceApp.repository;
 
+import com.java.EcomerceApp.model.Order;
 import com.java.EcomerceApp.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Payment findByOrder(Order order);
 }
