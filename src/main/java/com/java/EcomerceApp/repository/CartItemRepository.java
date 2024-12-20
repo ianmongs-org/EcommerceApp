9a9f2ff5e553bf54f1f591be97a42dfeb94057ae
Dancan254 <dancanian25@gmail.com>
2024-12-20 17:31:01 +0300

    feat: Add method to delete all cart items by cart reference

--- src/main/java/com/java/EcomerceApp/repository/CartItemRepository.java
+++ src/main/java/com/java/EcomerceApp/repository/CartItemRepository.java
@@ -1,5 +1,6 @@
 package com.java.EcomerceApp.repository;
 
+import com.java.EcomerceApp.model.Cart;
 import com.java.EcomerceApp.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query("SELECT c FROM CartItem c WHERE c.cart.cartId = ?1 AND c.product.productId = ?2")
     CartItem findByCartIdAndProductId(Long cartId, Long productId);
+
+    void deleteAllByCart(Cart cart);
 }
