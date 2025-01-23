aa18bf6d8f959acb5528cf0ebaf1c5dafb59accf
Dancan254 <dancanian25@gmail.com>
2025-01-23 18:48:04 +0300

    feat: Update WebSecurityConfig to enhance API security and remove unused web security customizer

--- src/main/java/com/java/EcomerceApp/config/WebSecurityConfig.java
+++ src/main/java/com/java/EcomerceApp/config/WebSecurityConfig.java
@@ -11,8 +11,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,17 +22,17 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-    @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
-    @Autowired
     UserDetailsServiceImpl userDetailsService;
 
+    public WebSecurityConfig(AuthEntryPointJwt unauthorizedHandler, UserDetailsServiceImpl userDetailsService) {
+        this.unauthorizedHandler = unauthorizedHandler;
+        this.userDetailsService = userDetailsService;
+    }
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
-
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -52,13 +52,24 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("api/auth/**").permitAll()
-                                .requestMatchers("v3/api-docs/**", "swagger-ui/**", "swagger-ui.html").permitAll()
+                        auth
+                                .requestMatchers("api/auth/**").permitAll()
+                                .requestMatchers("/api/v1/auth/**",
+                                 "/v2/api-docs",
+                                 "/v3/api-docs",
+                                 "/v3/api-docs/**",
+                                 "/swagger-resources",
+                                 "/swagger-resources/**",
+                                 "/configuration/ui",
+                                 "/configuration/security",
+                                 "/swagger-ui/**",
+                                 "/webjars/**",
+                                 "/swagger-ui.html").permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers("api/public/**").permitAll()
                                 .requestMatchers("api/test/**").permitAll()
@@ -67,14 +78,14 @@ public class WebSecurityConfig {
                 );
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return http.build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**", "/swagger-ui/**",
-                "/swagger-ui.html", "/configuration/ui",
-                "/configuration/security", "/webjars/**");
-    }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**", "/swagger-ui/**",
+//                "/swagger-ui.html", "/configuration/ui",
+//                "/configuration/security", "/webjars/**");
+//    }
 }
