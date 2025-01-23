d931d922039bf466eb2ea3c37247ada6004a6f16
Dancan254 <dancanian25@gmail.com>
2025-01-23 18:48:45 +0300

    feat: Add OpenAPI annotations to OrderController for improved API documentation

--- src/main/java/com/java/EcomerceApp/controller/OrderController.java
+++ src/main/java/com/java/EcomerceApp/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.java.EcomerceApp.dto.OrderDTO;
 import com.java.EcomerceApp.dto.OrderRequestDTO;
 import com.java.EcomerceApp.security.utils.AuthUtil;
 import com.java.EcomerceApp.service.orders.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class OrderController {
     private AuthUtil authUtil;
 
     @PostMapping("/placeOrder/users/payments/{paymentMethod}")
+    @Operation(summary = "Place an order", description = "Place an order with the specified payment method")
     public ResponseEntity<OrderDTO> placeOrder(@PathVariable String paymentMethod, @RequestBody OrderRequestDTO orderRequestDTO) {
         String emailId = authUtil.loggedInUserEmail();
 
