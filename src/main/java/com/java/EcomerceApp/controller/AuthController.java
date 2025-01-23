6ebc100d4fcb76a3d4e4d2b5c6c7c0cce57f4a1c
Dancan254 <dancanian25@gmail.com>
2025-01-23 18:48:22 +0300

    feat: Add OpenAPI annotations to AuthController for enhanced API documentation

--- src/main/java/com/java/EcomerceApp/controller/AuthController.java
+++ src/main/java/com/java/EcomerceApp/controller/AuthController.java
@@ -17,15 +17,19 @@ import com.java.EcomerceApp.dto.SignupRequest;
 import com.java.EcomerceApp.dto.UserInfoResponse;
 import com.java.EcomerceApp.security.services.AuthService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/auth")
+@Tag(name = "Authentication", description = "Authentication APIs")
 public class AuthController {
     @Autowired
     AuthService authService;
 
     @PostMapping("/signin")
+    @Operation(summary = "Sign in", description = "Authenticate user and return user information")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest){
         try {
             UserInfoResponse loginResponse = authService.authenticateUser(loginRequest);
@@ -39,6 +43,7 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
+    @Operation(summary = "Sign up", description = "Register a new user")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
         try {
             return new ResponseEntity<>(authService.registerUser(signUpRequest), HttpStatus.OK);
@@ -49,6 +54,4 @@ public class AuthController {
             return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
         }
     }
-
-    
 }
