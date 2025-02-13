067da2193981075bde8b18f9eb1b4020791fe4ed
Dancan254 <dancanian25@gmail.com>
2025-02-13 09:54:56 +0300

    refactor: Replace @Data with @Getter and @Setter in OrderDTO, OrderItemDTO, and OrderRequestDTO for improved clarity

--- src/main/java/com/java/EcomerceApp/dto/OrderDTO.java
+++ src/main/java/com/java/EcomerceApp/dto/OrderDTO.java
@@ -1,13 +1,12 @@
 package com.java.EcomerceApp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDTO {
