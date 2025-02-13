68034f6c77408c8f67bfa9cacc3177588b1decc4
Dancan254 <dancanian25@gmail.com>
2025-02-13 09:45:22 +0300

    refactor: Replace field injection with constructor injection in OrderController for improved testability

--- src/main/java/com/java/EcomerceApp/controller/OrderController.java
+++ src/main/java/com/java/EcomerceApp/controller/OrderController.java
@@ -5,7 +5,6 @@ import com.java.EcomerceApp.dto.OrderRequestDTO;
 import com.java.EcomerceApp.security.utils.AuthUtil;
 import com.java.EcomerceApp.service.orders.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,16 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/order")
 public class OrderController {
-    @Autowired
-    private OrderService orderService;
-    @Autowired
-    private AuthUtil authUtil;
+    private final OrderService orderService;
+    private final AuthUtil authUtil;
 
-    @PostMapping("/placeOrder/users/payments/{paymentMethod}")
+    public OrderController(OrderService orderService, AuthUtil authUtil){
+        this.orderService = orderService;
+        this.authUtil = authUtil;
+    }
+
+    @PostMapping("/users/payments/{paymentMethod}")
     @Operation(summary = "Place an order", description = "Place an order with the specified payment method")
     public ResponseEntity<OrderDTO> placeOrder(@PathVariable String paymentMethod, @RequestBody OrderRequestDTO orderRequestDTO) {
         String emailId = authUtil.loggedInUserEmail();
-
         OrderDTO orderDTO = orderService.placeOrder(emailId, orderRequestDTO);
         return new ResponseEntity<>(orderDTO, HttpStatus.CREATED);
     }
