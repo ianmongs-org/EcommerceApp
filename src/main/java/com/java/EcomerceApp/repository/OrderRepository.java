e3b8c35022b12d85e105ae70842bb3c110d2cf7e
Dancan254 <dancanian25@gmail.com>
2025-05-24 05:53:05 +0300

    fix: Correct parameter name in existsByEmailAndAddressAndOrderDate method in OrderRepository

--- src/main/java/com/java/EcomerceApp/repository/OrderRepository.java
+++ src/main/java/com/java/EcomerceApp/repository/OrderRepository.java
@@ -2,10 +2,17 @@ package com.java.EcomerceApp.repository;
 
 import com.java.EcomerceApp.model.Address;
 import com.java.EcomerceApp.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    boolean existsByEmailAndAddressAndOrderDate(String email, Address address, LocalDate now);
+    boolean existsByEmailAndAddressAndOrderDate(String email, Address address, LocalDate orderDate);
+    List<Order> findByEmailOrderByOrderDateDesc(String email);
+    Page<Order> findAll(Pageable pageable);
 }
