6cb04e730127e1c496dffed859e1e8e0edb23491
Dancan254 <dancanian25@gmail.com>
2025-05-24 05:54:11 +0300

    feat: Add methods to CartService interface for product management in the cart

--- src/main/java/com/java/EcomerceApp/service/cart/CartService.java
+++ src/main/java/com/java/EcomerceApp/service/cart/CartService.java
@@ -1,9 +1,16 @@
 package com.java.EcomerceApp.service.cart;
 
 import com.java.EcomerceApp.dto.CartDTO;
+import com.java.EcomerceApp.model.Cart;
 
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
 
     CartDTO getCart();
+
+    CartDTO removeProductFromCart(Long productId);
+
+    CartDTO updateProductQuantity(Long productId, Integer quantity);
+
+    void clearCart();
 }
