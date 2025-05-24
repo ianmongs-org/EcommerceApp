9aef5384cb0703528c5aa5a453f67ef7a5349ab8
Dancan254 <dancanian25@gmail.com>
2025-05-24 05:52:55 +0300

    feat: Add StkPushResponse class for handling STK push response data

---
+++ src/main/java/com/java/EcomerceApp/payment/StkPushResponse.java
@@ -0,0 +1,24 @@
+package com.java.EcomerceApp.payment;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class StkPushResponse {
+
+    @JsonProperty("MerchantRequestID")
+    private String merchantRequestID;
+
+    @JsonProperty("CheckoutRequestID")
+    private String checkoutRequestID;
+
+    @JsonProperty("ResponseCode")
+    private String responseCode;
+
+    @JsonProperty("ResponseDescription")
+    private String responseDescription;
+
+    @JsonProperty("CustomerMessage")
+    private String customerMessage;
+
+}
