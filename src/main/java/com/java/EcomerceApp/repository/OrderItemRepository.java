218b9e0a833f1e6a5f492a78a900bc2ff864a532
Dancan254 <dancanian25@gmail.com>
2025-05-24 05:53:01 +0300

    feat: Add method to find OrderItems by Order in OrderItemRepository

--- src/main/java/com/java/EcomerceApp/repository/OrderItemRepository.java
+++ src/main/java/com/java/EcomerceApp/repository/OrderItemRepository.java
@@ -1,7 +1,13 @@
 package com.java.EcomerceApp.repository;
 
+import com.java.EcomerceApp.model.Order;
 import com.java.EcomerceApp.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrder(Order order);
 }
