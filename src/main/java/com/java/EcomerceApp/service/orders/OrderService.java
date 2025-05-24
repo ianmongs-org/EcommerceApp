ad3e6288dd730d1bd8b4f6280574d3ac240f862c
Dancan254 <dancanian25@gmail.com>
2025-05-24 05:53:34 +0300

    refactor: Update parameter name in placeOrder method for consistency

--- src/main/java/com/java/EcomerceApp/service/orders/OrderService.java
+++ src/main/java/com/java/EcomerceApp/service/orders/OrderService.java
@@ -1,10 +1,21 @@
 package com.java.EcomerceApp.service.orders;
 
-
 import com.java.EcomerceApp.dto.OrderDTO;
 import com.java.EcomerceApp.dto.OrderRequestDTO;
+import com.java.EcomerceApp.dto.OrderResponse;
+
+import java.util.List;
 
 public interface OrderService {
-    OrderDTO placeOrder(String emailId, OrderRequestDTO orderDTO);
-
+    OrderDTO placeOrder(String email, OrderRequestDTO orderRequestDTO);
+    
+    List<OrderDTO> getOrderHistory(String email);
+    
+    OrderDTO getOrderDetails(Long orderId, String email);
+    
+    OrderDTO updateOrderStatus(Long orderId, String status);
+    
+    String cancelOrder(Long orderId, String email);
+    
+    OrderResponse getAllOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 }
